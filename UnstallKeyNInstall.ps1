bc7ca3cfa666732cdce9f035f9c4dca9101be440
Peter Cheung <takshing2001@hotmail.com>
2024-06-25 14:40:23 +0800

    Add files via upload

---
+++ UnstallKeyNInstall.ps1
@@ -0,0 +1,8 @@
+﻿# Uninstall the current license
+slmgr /upk
+
+# Retrieve the original product key and store it in a variable
+$OriginalProductKey = (wmic path softwarelicensingservice get OA3xOriginalProductKey | findstr /v "OA3xOriginalProductKey").Trim()
+
+# Install the license using the retrieved key
+slmgr /ipk $OriginalProductKey
